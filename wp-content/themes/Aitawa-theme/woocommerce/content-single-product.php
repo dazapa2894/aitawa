1847d00e6c3541ac0aea4f072776b40bd075a8f6
David <dazapa2894@gmail.com>
2020-08-19 12:35:39 -0500

    agregue comentarios y productos relacionados a la pagina de productos

--- wp-content/themes/Aitawa-theme/woocommerce/content-single-product.php
+++ wp-content/themes/Aitawa-theme/woocommerce/content-single-product.php
@@ -155,26 +155,13 @@ if (post_password_required()) {
 
 	</section>
 
-
-
-
-	<div class="summary entry-summary">
+	<section id="comentarios">
+		<!--VVV INICIO CONTENIDO CREADO POR WP VVV-->
 		<?php
-		/**
-		 * Hook: woocommerce_single_product_summary.
-		 *
-		 * @hooked woocommerce_template_single_title - 5
-		 * @hooked woocommerce_template_single_rating - 10
-		 * @hooked woocommerce_template_single_price - 10
-		 * @hooked woocommerce_template_single_excerpt - 20
-		 * @hooked woocommerce_template_single_add_to_cart - 30
-		 * @hooked woocommerce_template_single_meta - 40
-		 * @hooked woocommerce_template_single_sharing - 50
-		 * @hooked WC_Structured_Data::generate_product_data() - 60
-		 */
-		//do_action('woocommerce_single_product_summary');
+		wc_get_template('single-product/tabs/custom-reviews.php'); 
 		?>
-	</div>
+		<!--^^^ FIN CONTENIDO CREADO POR WP ^^^-->
+	</section>
 
 	<?php
 	/**
@@ -184,7 +171,8 @@ if (post_password_required()) {
 	 * @hooked woocommerce_upsell_display - 15
 	 * @hooked woocommerce_output_related_products - 20
 	 */
-	//do_action( 'woocommerce_after_single_product_summary' );
+	remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs' );
+	do_action( 'woocommerce_after_single_product_summary' );
 	?>
 </div>
 
